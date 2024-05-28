feac0ed30ed0836174f969f1dcb2c1820191a79f
alexander <alexander@DESKTOP-31GTKR0>
2024-05-28 20:20:21 +0200

    Add project files.

---
+++ WinFormsApp1/Form1.Designer.vb
@@ -0,0 +1,242 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()>
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()>
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()>
+    Private Sub InitializeComponent()
+        btnShowPromotionalOffer = New Button()
+        btnClear = New Button()
+        btnExit = New Button()
+        radXbox = New RadioButton()
+        radPlayStation = New RadioButton()
+        Label1 = New Label()
+        Label2 = New Label()
+        Label3 = New Label()
+        Label4 = New Label()
+        txtName = New TextBox()
+        txtSurname = New TextBox()
+        txtCellphone = New TextBox()
+        txtEmail = New TextBox()
+        LstQuote = New ListBox()
+        chkHeadset = New CheckBox()
+        chkController = New CheckBox()
+        chkMonitor = New CheckBox()
+        SuspendLayout()
+        ' 
+        ' btnShowPromotionalOffer
+        ' 
+        btnShowPromotionalOffer.Location = New Point(106, 348)
+        btnShowPromotionalOffer.Name = "btnShowPromotionalOffer"
+        btnShowPromotionalOffer.Size = New Size(122, 23)
+        btnShowPromotionalOffer.TabIndex = 0
+        btnShowPromotionalOffer.Text = "Promotional Offer"
+        btnShowPromotionalOffer.UseVisualStyleBackColor = True
+        ' 
+        ' btnClear
+        ' 
+        btnClear.Location = New Point(258, 348)
+        btnClear.Name = "btnClear"
+        btnClear.Size = New Size(75, 23)
+        btnClear.TabIndex = 1
+        btnClear.Text = "Cl&ear"
+        btnClear.UseVisualStyleBackColor = True
+        ' 
+        ' btnExit
+        ' 
+        btnExit.Location = New Point(407, 348)
+        btnExit.Name = "btnExit"
+        btnExit.Size = New Size(75, 23)
+        btnExit.TabIndex = 2
+        btnExit.Text = "Ex&it"
+        btnExit.UseVisualStyleBackColor = True
+        ' 
+        ' radXbox
+        ' 
+        radXbox.AutoSize = True
+        radXbox.Location = New Point(152, 207)
+        radXbox.Name = "radXbox"
+        radXbox.Size = New Size(97, 19)
+        radXbox.TabIndex = 3
+        radXbox.TabStop = True
+        radXbox.Text = "RadioButton1"
+        radXbox.UseVisualStyleBackColor = True
+        ' 
+        ' radPlayStation
+        ' 
+        radPlayStation.AutoSize = True
+        radPlayStation.Location = New Point(152, 257)
+        radPlayStation.Name = "radPlayStation"
+        radPlayStation.Size = New Size(97, 19)
+        radPlayStation.TabIndex = 4
+        radPlayStation.TabStop = True
+        radPlayStation.Text = "RadioButton2"
+        radPlayStation.UseVisualStyleBackColor = True
+        ' 
+        ' Label1
+        ' 
+        Label1.AutoSize = True
+        Label1.Location = New Point(70, 35)
+        Label1.Name = "Label1"
+        Label1.Size = New Size(41, 15)
+        Label1.TabIndex = 5
+        Label1.Text = "Label1"
+        ' 
+        ' Label2
+        ' 
+        Label2.AutoSize = True
+        Label2.Location = New Point(70, 80)
+        Label2.Name = "Label2"
+        Label2.Size = New Size(41, 15)
+        Label2.TabIndex = 6
+        Label2.Text = "Label2"
+        ' 
+        ' Label3
+        ' 
+        Label3.AutoSize = True
+        Label3.Location = New Point(292, 35)
+        Label3.Name = "Label3"
+        Label3.Size = New Size(41, 15)
+        Label3.TabIndex = 7
+        Label3.Text = "Label3"
+        ' 
+        ' Label4
+        ' 
+        Label4.AutoSize = True
+        Label4.Location = New Point(292, 80)
+        Label4.Name = "Label4"
+        Label4.Size = New Size(41, 15)
+        Label4.TabIndex = 8
+        Label4.Text = "Label4"
+        ' 
+        ' txtName
+        ' 
+        txtName.Location = New Point(157, 27)
+        txtName.Name = "txtName"
+        txtName.Size = New Size(100, 23)
+        txtName.TabIndex = 9
+        ' 
+        ' txtSurname
+        ' 
+        txtSurname.Location = New Point(157, 73)
+        txtSurname.Name = "txtSurname"
+        txtSurname.Size = New Size(100, 23)
+        txtSurname.TabIndex = 10
+        ' 
+        ' txtCellphone
+        ' 
+        txtCellphone.Location = New Point(382, 35)
+        txtCellphone.Name = "txtCellphone"
+        txtCellphone.Size = New Size(100, 23)
+        txtCellphone.TabIndex = 11
+        ' 
+        ' txtEmail
+        ' 
+        txtEmail.Location = New Point(382, 74)
+        txtEmail.Name = "txtEmail"
+        txtEmail.Size = New Size(100, 23)
+        txtEmail.TabIndex = 12
+        ' 
+        ' LstQuote
+        ' 
+        LstQuote.FormattingEnabled = True
+        LstQuote.ItemHeight = 15
+        LstQuote.Location = New Point(522, 207)
+        LstQuote.Name = "LstQuote"
+        LstQuote.Size = New Size(204, 229)
+        LstQuote.TabIndex = 13
+        ' 
+        ' chkHeadset
+        ' 
+        chkHeadset.AutoSize = True
+        chkHeadset.Location = New Point(373, 200)
+        chkHeadset.Name = "chkHeadset"
+        chkHeadset.Size = New Size(85, 19)
+        chkHeadset.TabIndex = 14
+        chkHeadset.Text = "CheckBox1"
+        chkHeadset.UseVisualStyleBackColor = True
+        ' 
+        ' chkController
+        ' 
+        chkController.AutoSize = True
+        chkController.Location = New Point(373, 225)
+        chkController.Name = "chkController"
+        chkController.Size = New Size(85, 19)
+        chkController.TabIndex = 15
+        chkController.Text = "CheckBox2"
+        chkController.UseVisualStyleBackColor = True
+        ' 
+        ' chkMonitor
+        ' 
+        chkMonitor.AutoSize = True
+        chkMonitor.Location = New Point(373, 250)
+        chkMonitor.Name = "chkMonitor"
+        chkMonitor.Size = New Size(85, 19)
+        chkMonitor.TabIndex = 16
+        chkMonitor.Text = "CheckBox3"
+        chkMonitor.UseVisualStyleBackColor = True
+        ' 
+        ' Form1
+        ' 
+        AutoScaleDimensions = New SizeF(7F, 15F)
+        AutoScaleMode = AutoScaleMode.Font
+        ClientSize = New Size(800, 450)
+        Controls.Add(chkMonitor)
+        Controls.Add(chkController)
+        Controls.Add(chkHeadset)
+        Controls.Add(LstQuote)
+        Controls.Add(txtEmail)
+        Controls.Add(txtCellphone)
+        Controls.Add(txtSurname)
+        Controls.Add(txtName)
+        Controls.Add(Label4)
+        Controls.Add(Label3)
+        Controls.Add(Label2)
+        Controls.Add(Label1)
+        Controls.Add(radPlayStation)
+        Controls.Add(radXbox)
+        Controls.Add(btnExit)
+        Controls.Add(btnClear)
+        Controls.Add(btnShowPromotionalOffer)
+        Name = "Form1"
+        Text = "Form1"
+        ResumeLayout(False)
+        PerformLayout()
+    End Sub
+
+    Friend WithEvents btnShowPromotionalOffer As Button
+    Friend WithEvents btnClear As Button
+    Friend WithEvents btnExit As Button
+    Friend WithEvents radXbox As RadioButton
+    Friend WithEvents radPlayStation As RadioButton
+    Friend WithEvents Label1 As Label
+    Friend WithEvents Label2 As Label
+    Friend WithEvents Label3 As Label
+    Friend WithEvents Label4 As Label
+    Friend WithEvents txtName As TextBox
+    Friend WithEvents txtSurname As TextBox
+    Friend WithEvents txtCellphone As TextBox
+    Friend WithEvents txtEmail As TextBox
+    Friend WithEvents LstQuote As ListBox
+    Friend WithEvents chkHeadset As CheckBox
+    Friend WithEvents chkController As CheckBox
+    Friend WithEvents chkMonitor As CheckBox
+
+End Class
---
+++ WinFormsApp1/Form1.vb
@@ -0,0 +1,151 @@
+﻿Public Class Form1
+
+    ' Product Prices
+    Private Const XboxPrice As Decimal = 9999D
+    Private Const PlayStationPrice As Decimal = 12999D
+    Private Const HeadsetPrice As Decimal = 349D
+    Private Const ControllerPrice As Decimal = 450D
+    Private Const MonitorPrice As Decimal = 1999D
+
+    ' VAT Rate
+    Private Const VatRate As Decimal = 0.15D
+
+    ' Discount Rates
+    Private Const DiscountRate1 As Decimal = 0.2D
+    Private Const DiscountRate2 As Decimal = 0.25D
+    Private Const DiscountRate3 As Decimal = 0.3D
+
+    Private Sub btnShowPromotionalOffer_Click(sender As Object, e As EventArgs) Handles btnShowPromotionalOffer.Click
+
+        ' Validate user selection
+        If Not radXbox.Checked And Not radPlayStation.Checked Then
+            MessageBox.Show("Please select a product from Group A.")
+            Return
+        End If
+
+        ' Calculate amounts
+        Dim totalAmount As Decimal = CalculateTotalAmount()
+        Dim discountAmount As Decimal = CalculateDiscountAmount()
+        Dim vatAmount As Decimal = CalculateVatAmount(discountAmount)
+        Dim finalAmount As Decimal = CalculateFinalAmount(totalAmount, discountAmount, vatAmount)
+
+        ' Generate Quote
+        GenerateQuote(totalAmount, discountAmount, vatAmount, finalAmount)
+    End Sub
+
+    Private Function CalculateTotalAmount() As Decimal
+
+        Dim groupAPrice As Decimal
+        If radXbox.Checked Then
+            groupAPrice = XboxPrice
+        ElseIf radPlayStation.Checked Then
+            groupAPrice = PlayStationPrice
+        End If
+
+        Dim groupBTotal As Decimal = 0
+        If chkHeadset.Checked Then
+            groupBTotal += HeadsetPrice
+        End If
+        If chkController.Checked Then
+            groupBTotal += ControllerPrice
+        End If
+        If chkMonitor.Checked Then
+            groupBTotal += MonitorPrice
+        End If
+
+        Return groupAPrice + groupBTotal
+    End Function
+
+    Private Function CalculateDiscountAmount() As Decimal
+        Dim totalGroupB As Decimal = 0
+        Dim totalItemsB As Integer = 0
+
+        If chkHeadset.Checked Then
+            totalGroupB += HeadsetPrice
+            totalItemsB += 1
+        End If
+        If chkController.Checked Then
+            totalGroupB += ControllerPrice
+            totalItemsB += 1
+        End If
+        If chkMonitor.Checked Then
+            totalGroupB += MonitorPrice
+            totalItemsB += 1
+        End If
+
+        ' Determine discount rate based on number of items in Group B
+        Dim discountRate As Decimal = 0D
+        Select Case totalItemsB
+            Case 1
+                discountRate = DiscountRate1
+
+            Case 2
+                discountRate = DiscountRate2
+
+            Case Is >= 3
+                discountRate = DiscountRate3
+
+        End Select
+
+        Return totalGroupB * discountRate
+
+    End Function
+
+    Private Function CalculateVatAmount(discountAmount As Decimal) As Decimal
+        Dim totalAmount As Decimal = CalculateTotalAmount()
+        Dim taxableAmount As Decimal = totalAmount - discountAmount
+        Return taxableAmount * VatRate
+    End Function
+
+    Private Function CalculateFinalAmount(totalAmount As Decimal, discountAmount As Decimal, vatAmount As Decimal) As Decimal
+        Return totalAmount - discountAmount + vatAmount
+    End Function
+
+    Private Sub GenerateQuote(totalAmount As Decimal, discountAmount As Decimal, vatAmount As Decimal, finalAmount As Decimal)
+
+        LstQuote.Items.Clear()
+
+        LstQuote.Items.Add("Customer Details:")
+        LstQuote.Items.Add($"Name: {txtName.Text}")
+        LstQuote.Items.Add($"Surname: {txtSurname.Text}")
+        LstQuote.Items.Add($"Cellphone: {txtCellphone.Text}")
+        LstQuote.Items.Add($"Email: {txtEmail.Text}")
+        LstQuote.Items.Add("")
+        LstQuote.Items.Add("Promotional Offer Details:")
+        LstQuote.Items.Add($"Total Amount: R{totalAmount:0.00}")
+        LstQuote.Items.Add($"Discount Amount: R{discountAmount:0.00}")
+        LstQuote.Items.Add($"VAT Amount: R{vatAmount:0.00}")
+        LstQuote.Items.Add($"Final Amount Payable: R{finalAmount:0.00}")
+    End Sub
+
+    Private Sub btnClear_Click(sender As Object, e As EventArgs) Handles btnClear.Click
+
+        ' Clear all selections and inputs
+        radXbox.Checked = False
+        radPlayStation.Checked = False
+        chkHeadset.Checked = False
+        chkController.Checked = False
+        chkMonitor.Checked = False
+
+        txtName.Clear()
+        txtSurname.Clear()
+        txtCellphone.Clear()
+        txtEmail.Clear()
+        LstQuote.Items.Clear()
+    End Sub
+
+    Private Sub btnExit_Click(sender As Object, e As EventArgs) Handles btnExit.Click
+
+        ' Exit the application
+        Me.Close()
+
+    End Sub
+
+End Class
+
+
+
+
+
+
+
